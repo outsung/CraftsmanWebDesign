39c2f1905129402636dc6af6de3ad3b3e802aa3f
outsung <40460655+outsung@users.noreply.github.com>
2019-12-04 09:15:39 +0900

    12.04

---
+++ web03/js/common.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+
+$(function () {
+ $('header nav > ul > li > ul').hide();
+ $('header nav > ul > li > a ').on('mouseenter', function(){
+  $(this).addClass('on').next().stop().slideDown();
+ });
+ $('header nav > ul > li').on('mouseleave', function(){
+  $(this).children('a').removeClass('on').next().stop().slideUp(200);
+ });
+})
+
+
+$(function () {
+    $('.popuplink').on('click', function () {
+        $('.popup').fadeIn();
+    })
+    $('.popup button').on('click', function () {
+        $('.popup').fadeOut();
+    })
+})
+
+$(function () {
+	var now = 1;
+	setInterval(function() {
+	if (now == 3) {
+	now = 1;
+	} else {
+	 now += 1;
+	}
+	$('.slide ul li').hide();
+	$('.slide ul li').eq(now-1).fadeIn();
+	}, 3000)
+})
+
+
+$(function () {
+	$('.c1 h3').on('click', function () {
+        $(this).addClass('on').next().show();
+        $(this).parent('div').siblings('div')
+            .children('h3').removeClass('on')
+            .next().hide();
+    })
+})
